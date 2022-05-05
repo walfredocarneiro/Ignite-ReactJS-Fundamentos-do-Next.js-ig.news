8bf17af389efaa3446f432fde0e87507ca334734
Walfredo Carneiro <walfredo.carneiro@gmail.com>
2022-05-05 02:45:35 +0000

    New global.css

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+export default class Mydocument extends Document {
+    render(){
+        return(
+            <Html>
+                <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,900;1,400;1,700&display=swap" rel="stylesheet" /> 
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
