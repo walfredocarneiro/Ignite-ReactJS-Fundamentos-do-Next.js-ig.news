7c46b26926667559e8b03f56ba115c8a130464d7
Walfredo Carneiro <walfredo.carneiro@gmail.com>
2022-05-05 21:22:14 +0000

    included Signin Button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
 import styles from './styles.module.scss';
+import { SigninButton } from '../SigninButton';
 
-export function Header(){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
-            </div>
-        </header>
-    );
+export function Header() {
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<img src="/images/logo.svg" alt="ig.news" />
+				<nav>
+					<a className={styles.active}>Home</a>
+					<a>Posts</a>
+				</nav>
+				<SigninButton />
+			</div>
+		</header>
+	);
 }
