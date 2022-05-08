3ee18787e9d98a1df415b6c4a602301c4aebd6f8
Walfredo Carneiro <walfredo.carneiro@gmail.com>
2022-05-08 00:45:34 +0000

    GitHub

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,4 +19,24 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true
+      } catch{
+        return false
+      }
+    },
+  },
 })
